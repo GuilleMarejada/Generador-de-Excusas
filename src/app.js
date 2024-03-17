f0ca5af510b405302b57d21dea2d8a16459b5f52
Guillermo C.G <151522926+GuilleMarejada@users.noreply.github.com>
2024-03-17 14:21:48 +0000

    Update app.js

--- src/app.js
+++ src/app.js
@@ -6,6 +6,24 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  
+function creadorDeExcusas() {
+    const who = ['Mi perro', 'Mi abuela', 'Su tortuga', 'Mi pájaro'];
+    const action = ['se comió', 'se orinó en', 'aplastó', 'rompió'];
+    const what = ['mi tarea', 'las llaves', 'el coche'];
+    const when = ['antes de la clase', 'justo a tiempo', 'cuando terminé', 'durante mi almuerzo', 'mientras programaba'];
+
+    let randomWho = who[Math.floor(Math.random() * who.length)];
+    let randomAction = action[Math.floor(Math.random() * action.length)];
+    let randomWhat = what[Math.floor(Math.random() * what.length)];
+    let randomWhen = when[Math.floor(Math.random() * when.length)];
+
+    let phrase =randomWho.concat(' ',randomAction, ' ',randomWhat, ' ',randomWhen,"!!");
+
+    document.getElementById("excuse").innerHTML = phrase;
+}
+
+document.getElementById("crear").addEventListener("click", creadorDeExcusas); 
+  
   console.log("Generando excusas");
 };
